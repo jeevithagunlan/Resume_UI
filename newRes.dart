1d7382d25571a02d04eea0bbb8c6f9584627b673
jeevithagunlan <jeevithaguan1810@gmail.com>
2025-05-25 12:14:32 +0530

    modified

--- newRes.dart
+++ newRes.dart
@@ -1,1 +1,3 @@
-import 'newRes.dart';
+import 'package:flutter/ResumeUI/new_file.dart';
+
+// TODO: Update the import above to point to an existing file or create 'new_file.dart' in the correct location.
